76030d7f7dc4038cfbd3fd07d3e3225b4504402c
unknown <s.medykowski@gmail.com>
2019-07-09 14:08:48 +0200

    Adding House class to directory builder/house

---
+++ Patterns/src/main/java/builder/house/House.java
@@ -0,0 +1,10 @@
+package builder.house;
+
+public class House {
+    private String walls;
+    private String floors;
+    private String rooms;
+    private String windows;
+    private String doors;
+    private String garage;
+}
